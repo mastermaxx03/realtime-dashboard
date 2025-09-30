87ab1d9f66b9b279cd115a3a9bf6cef947cd1253
mastermaxx03 <srivastavaanimesh22@gmail.com>
2025-09-30 17:04:47 +0530

    added segmented control

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,10 @@
-import { RouterProvider } from 'react-router-dom';
-
-// routing
-import router from 'routes';
-
-// project imports
-
+import RealtimeLayoutPage2 from './views/realtime-layout2';
 import ThemeCustomization from 'themes';
-import { setupAutoRefresh } from 'utils/idleAndNavigationRefresh';
-
-// auth provider
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-// offline lock hook (adds blur & disables app when offline)
-
-// toast outside #root so it never blurs
-import ToastOutsideRoot from 'ui/ToastOutsideRoot';
-
-// ==============================|| APP ||============================== //
 
 export default function App() {
-  useOfflineLock();
-
-  setupAutoRefresh(router);
-
   return (
     <ThemeCustomization>
-      <MachineStatusProvider>
-        <NavigationScroll>
-          <RouterProvider router={router} />
-          <ToastContainer autoClose={2500} />
-        </NavigationScroll>
-
-        <ToastOutsideRoot />
-      </MachineStatusProvider>
+      <RealtimeLayoutPage2 />
     </ThemeCustomization>
   );
 }
